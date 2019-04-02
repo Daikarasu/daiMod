9b133763dd7fc8cedc67f0a6c7c1166b3d303414
Daikarasu <Daikarasu#9393>
2019-04-02 15:43:00 +0200

    Le Sanae LoLK
    Happy late april fools

---
+++ le_sanae/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "le_sanae",
+	"servers": [
+		"https://raw.githubusercontent.com/Daikarasu/daiMod/master/le_sanae/"
+	],
+	"title": "Replaces every single texture in LoLK to LE SANAE. Viewer discretion is advised. (April Fools 2019)"
+}
--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
 	"contact": "Daikarasu#9393",
 	"id": "Daikarasu",
 	"patches": {
-		"RNG": "Randomized Danmaku for IN, MoF, SA, UFO, TD, DDC, LoLK and HSiFS."
+		"RNG": "Randomized Danmaku for IN, MoF, SA, UFO, TD, DDC, LoLK and HSiFS.",
+		"le_sanae": "Replaces every single texture in LoLK to LE SANAE. Viewer discretion is advised. (April Fools 2019)"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/Daikarasu/daiMod/master/"
