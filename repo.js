2d8c4807e502175c591ade4714423c561f1ba83f
Daikarasu <Daikarasu#9393>
2019-12-23 20:53:04 +0100

    RNG Patch 2.0 Release!
    Video: https://www.youtube.com/watch?v=c7ZRwwDU-vM

--- repo.js
+++ repo.js
@@ -1,11 +1,13 @@
 {
-	"contact": "Daikarasu#9393",
+	"contact": "Dai#9393",
 	"id": "Daikarasu",
 	"patches": {
-		"RNG": "(th08, 10, 11, 12, 12.5, 13, 14, 15, 16, 17) Makes every attack purely RNG.",
+		"RNG": "(v2.0) (th08, 10, 11, 12, 12.5, 13, 14, 15, 16, 17) Makes every attack purely RNG.",
+		"RNG-Legacy": "Old version of the RNG Patch. Use this to view replays made with this version.",
 		"le_sanae": "Replaces every single texture in LoLK to LE SANAE. Viewer discretion is advised. (April Fools 2019)"
 	},
 	"servers": [
+		"https://mirrors.thpatch.net/Daikarasu",
 		"https://raw.githubusercontent.com/Daikarasu/daiMod/master/"
 	],
 	"title": "Daikarasu's repository of Touhou patches"
