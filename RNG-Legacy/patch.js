f0c2a3bb3abf8b2cc960be700201a803609685d3
Daikarasu <Daikarasu#9393>
2019-12-23 21:06:43 +0100

    fix stuff

---
+++ RNG-Legacy/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "RNG",
+	"servers": [
+		"https://raw.githubusercontent.com/Daikarasu/daiMod/master/RNG-Legacy/"
+	],
+	"title": "Old version of the RNG Patch. Use this to view replays made with this version."
+}
--- RNG/patch.js
+++ RNG/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/Daikarasu/daiMod/master/RNG/"
 	],
-	"title": "(th08, 10, 11, 12, 12.5, 13, 14, 15, 16, 17) Makes every attack purely RNG."
+	"title": "(v2.0) (th08, 10, 11, 12, 12.5, 13, 14, 15, 16, 17) Makes every attack purely RNG."
 }
