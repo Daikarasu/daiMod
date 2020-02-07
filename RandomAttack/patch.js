b188bea230e23eee3d36875a891204130f7e3cf1
Daikarasu <Daikarasu#9393>
2020-02-07 15:32:01 +0100

    RandomAttack mod release

---
+++ RandomAttack/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "RandomAttack",
+	"servers": [
+		"https://mirrors.thpatch.net/Daikarasu/RandomAttack/",
+		"https://raw.githubusercontent.com/Daikarasu/daiMod/master/RandomAttack/"
+	],
+	"title": "(th15) Randomizes which attack gets fired every time. Chaos ensues."
+}
