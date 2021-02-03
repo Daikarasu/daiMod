a8b3a3cf04ae090de688cbf884c53f2f1b6f6727
Daikarasu <sander.j.veltman@gmail.com>
2021-02-03 15:12:18 +0100

    le_sanae slight description change again because I forgot something

--- le_sanae/files.js
+++ le_sanae/files.js
@@ -1,5 +1,5 @@
 {
-	"patch.js": 704274355,
+	"patch.js": 1594610416,
 	"th15/ascii/ascii.png": 334650801,
 	"th15/ascii/ascii_1280.png": 2100656290,
 	"th15/ascii/ascii_960.png": 2966153414,
--- le_sanae/patch.js
+++ le_sanae/patch.js
@@ -7,5 +7,5 @@
 		"https://mirrors.thpatch.net/Daikarasu/le_sanae/",
 		"https://raw.githubusercontent.com/Daikarasu/daiMod/master/le_sanae/"
 	],
-	"title": "Replaces almost every single texture in LoLK to LE SANAE. Viewer discretion is advised. (April Fools 2019)"
+	"title": "Replaces almost every single texture in LoLK to LE SANAE. Viewer discretion is advised."
 }
