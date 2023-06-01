e616d6b44d939230aa65178822ff171658156f5c
Daikarasu <47870703+Daikarasu@users.noreply.github.com>
2023-06-01 22:35:11 +0200

    RandomAttack (th14): v1.2.0, notable changes in desc
    -Several balancing changes, including chapter and spellcard adjustments.
    -The player is now given the option to fight the other stage 4 boss after beating Benben or Yatsuhashi.
    -The game now uses 32-bit RNG (using the patch Daikarasu/32bit_RNG)
    -The bullet cap has been increased to 10000 (using the patch ExpHP/bullet_cap)
    -Certain visuals in the stage background are now correctly reloaded after dying.
    -The reload effect that covers the screen momentarily now goes away faster.

--- RandomAttack/patch.js
+++ RandomAttack/patch.js
@@ -1,11 +1,16 @@
 {
 	"dependencies": [
-		"nmlgc/base_tsa"
+		"nmlgc/base_tsa",
+		"ExpHP/base_exphp",
+		"ExpHP/anm_leak",
+		"ExpHP/sprite_death_fix",
+		"ExpHP/bullet_cap",
+		"Daikarasu/32bit_RNG"
 	],
 	"id": "RandomAttack",
 	"servers": [
 		"https://mirrors.thpatch.net/Daikarasu/RandomAttack/",
 		"https://raw.githubusercontent.com/Daikarasu/daiMod/master/RandomAttack/"
 	],
-	"title": "(th14, 15) Randomizes which attack gets fired every time. Chaos ensues.\n    WARNING: th14 currently has unsolved random crashing for some users."
+	"title": "(th14, 15) Randomizes which attack gets fired every time. Chaos ensues."
 }
--- RandomAttack/th14.v1.00b.js
+++ RandomAttack/th14.v1.00b.js
@@ -1,9 +1,28 @@
 {
+    "options": {
+        "bullet-cap.bullet-cap": {
+            "type": "i32",
+            "val": 10000
+        },
+        "bullet-cap.laser-cap": {
+            "type": "i32",
+            "val": 2048
+        },
+        "bullet-cap.cancel-cap": {
+            "type": "i32",
+            "val": 16384
+        },
+    },
     "binhacks": {
-        "Dai.RandomAttack.th14.pointdevice.jmp_to_custom_ins_cave": {
+        "Dai.RandomAttack.th14.pointdevice.jmp_to_custom_ecl_ins_cave": {
             "addr": 0x4252DA,
             "expected": "0F87 [0x429C28]",
-            "code": "0F87 [codecave:Dai.RandomAttack.th14.pointdevice.custom_ins]"
+            "code": "0F87 [codecave:Dai.RandomAttack.th14.pointdevice.custom_ecl_ins]"
+        },
+        "Dai.RandomAttack.th14.pointdevice.jmp_to_custom_anm_ins_cave": {
+            "addr": 0x4728D3,
+            "expected": "0F87 [0x472AB9]",
+            "code": "0F87 [codecave:Dai.RandomAttack.th14.pointdevice.custom_anm_ins]"
         },
         "Dai.RandomAttack.th14.pointdevice.replace_on_player_death_func": {
             "addr": 0x44DD91,
@@ -19,6 +38,16 @@
             "addr": 0x41D4B3,
             "expected": "0F84 CB000000",
             "code": "0F84 [codecave:Dai.RandomAttack.th14.pointdevice.dont_display_spell_failed_popups_if_reload]"
+        },
+        "Dai.RandomAttack.th14.pointdevice.dont_reset_stage_camera_vec3_104_if_reload.jmp": {
+            "addr": 0x40D9CF,
+            "expected": "C74424 3C 00000000 C74424 40 00000000 F30F7E4424 3C 660FD686 78430000 C74424 44 00000000 8B4424 44 8986 80430000",
+            "code": "C74424 3C 00000000 C74424 40 00000000 C74424 44 00000000 E9 [codecave:Dai.RandomAttack.th14.pointdevice.dont_reset_stage_camera_vec3_104_if_reload] <int3:19>"
+        },
+        "Dai.RandomAttack.th14.pointdevice.make_unused_anm_vm_field_available": {
+            "addr": 0x470F71,
+            "expected": "8B87 4C050000 8987 A4040000",
+            "code": "<nop:12>"
         },
         
         "Dai.RandomAttack.th14.freeze_item_cycle": {
@@ -36,12 +65,23 @@
             "expected": "EB 0B 8360 18 FE C740 28 00000000 83C6 04 4F 75 DE 5F 5E 33C0 5B C3 <int3:1>",
             "code": "EB 0C 8360 18 FE 8360 1C DF 8348 1C 20 83C6 04 4F 75 DD 5F 5E 31C0 5B C3"
         },
+        "Dai.RandomAttack.th14.disable_sprite_transform_for_laser_line": {
+            "addr": 0x43B3C3,
+            "expected": "8B4C24 14 8DB7 14090000",
+            "code": "E9 [0x43B263] <nop:5>"
+        },
+        "Dai.RandomAttack.th14.disable_sprite_transform_for_laser_curve": {
+            "addr": 0x442195,
+            "expected": "8B4C24 14",
+            "code": "EB 3E <nop:2>"
+        },
         
         "Dai.RandomAttack.th14.unlock_extra": {
             "addr": 0x459220,
             "expected": "55 8BEC 51",
             "code": "31C0 40 C3"
         },
+        
     },
     "codecaves": {
         "Dai.RandomAttack.th14.pointdevice.enemy_ptr": {
@@ -58,11 +98,11 @@
         },
         "Dai.RandomAttack.th14.pointdevice.item_manager_snapshot": {
             "access": "rw",
-            "size": 0xDDD888,
+            "size": "(0x1C5888 + (<option:bullet-cap.cancel-cap> * 0xC18))",
         },
         "Dai.RandomAttack.th14.pointdevice.bullet_manager_snapshot": {
             "access": "rw",
-            "size": 0x9BF6D0,
+            "size": "(0x9C + (<option:bullet-cap.bullet-cap> * 0x13F4))",
         },
         "Dai.RandomAttack.th14.pointdevice.stage_background_snapshot": {
             "access": "rw",
@@ -83,20 +123,20 @@
             //0 = no action, 1 = create snapshot, 2 = load snapshot
         },
         
-        "Dai.RandomAttack.th14.pointdevice.custom_ins": {
+        "Dai.RandomAttack.th14.pointdevice.custom_ecl_ins": {
             //ins 2000 createSnapshot() - Save pointer to caller enemy, queue snapshot creation
             "access": "re",
-            "code": "3D A4060000 0F85 [0x429C28] 8B8F EC400000 890D <codecave:Dai.RandomAttack.th14.pointdevice.enemy_ptr> C705 <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 01000000 E9 [0x429C28]",
+            "code": "3D A4060000 0F85 [0x429C28] 8B8F EC400000 890D <codecave:Dai.RandomAttack.th14.pointdevice.enemy_ptr> C705 <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 01000000 8B0D <0x4DB67C> 6A 1E 8D89 C0820100 E8 [0x408B00] E9 [0x429C28]",
         },
         "Dai.RandomAttack.th14.pointdevice.on_player_death": {
             //unkill player, give 60 iframes to player, create reload screen effect, queue snapshot reload
             "access": "re",
-            "code": "C781 84060000 01000000 6A 3C 8D89 C0820100 E8 [0x408B00] 83EC 04 8D04E4 51 6A FF 8B0D <0x4DB53C> 68 C4000000 8B49 10 50 E8 [0x42DF70] 83C4 04 C705 <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 02000000 C3",
+            "code": "C781 84060000 01000000 6A 50 8D89 C0820100 E8 [0x408B00] 83EC 04 8D04E4 51 6A FF 8B0D <0x4DB53C> 68 C4000000 8B49 10 50 E8 [0x42DF70] 83C4 04 C705 <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 02000000 C3",
         },
         
         "Dai.RandomAttack.th14.pointdevice.check_snapshot_status": {
             "access": "re",
-            "code": "A1 <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 83F8 00 0F84 [0x436D1D] 83F8 01 75 07 E8 [codecave:Dai.RandomAttack.th14.pointdevice.create_snapshot] EB 0C 83F8 02 75 07 E8 [codecave:Dai.RandomAttack.th14.pointdevice.load_snapshot] <nop:2> C705 <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 00000000 E9 [0x436D1D]",
+            "code": "A1 <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 83F8 00 0F84 [0x436D1D] 83F8 01 75 07 E8 [codecave:Dai.RandomAttack.th14.pointdevice.create_snapshot] EB 11 83F8 02 75 0C E8 [codecave:Dai.RandomAttack.th14.pointdevice.load_snapshot] B8 03000000 EB 02 31C0 A3 <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> E9 [0x436D1D]",
         },
         "Dai.RandomAttack.th14.pointdevice.create_snapshot": {
             "access": "re",
@@ -104,7 +144,7 @@
         },
         "Dai.RandomAttack.th14.pointdevice.load_snapshot": {
             "access": "re",
-            "code": "56 57 53 8B35 <codecave:Dai.RandomAttack.th14.pointdevice.enemy_ptr> A1 <0x4DB544> 8BB8 D0000000 85FF 74 24 8B0F 8B5F 04 85C9 74 17 39F1 74 13 F781 44520000 00200000 75 07 8B01 6A 01 FF50 14 89DF EB D8 6A 00 6A 01 E8 [0x43AC00] E8 [0x41D450] 68 <codecave:Dai.RandomAttack.th14.pointdevice.enemy_snapshot> 56 E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_enemy] 68 <codecave:Dai.RandomAttack.th14.pointdevice.player_snapshot> FF35 <0x4DB67C> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_player] 68 <codecave:Dai.RandomAttack.th14.pointdevice.item_manager_snapshot> FF35 <0x4DB660> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_item_manager] 68 <codecave:Dai.RandomAttack.th14.pointdevice.bullet_manager_snapshot> FF35 <0x4DB530> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_bullet_manager] 68 <codecave:Dai.RandomAttack.th14.pointdevice.stage_background_snapshot> FF35 <0x4DB528> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_stage_background] 68 <codecave:Dai.RandomAttack.th14.pointdevice.globals_snapshot> 68 <0x4F5828> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_globals] 8B0D <0x4DB67C> E8 [0x44F970] 8B0D <0x4DB550> FF35 <0x4F5874> FF35 <0x4F5870> E8 [0x434FD0] 5B 5F 5E C3",
+            "code": "56 57 53 8B35 <codecave:Dai.RandomAttack.th14.pointdevice.enemy_ptr> A1 <0x4DB544> 8BB8 D0000000 85FF 74 24 8B0F 8B5F 04 85C9 74 17 39F1 74 13 F781 44520000 00200000 75 07 8B01 6A 01 FF50 14 89DF EB D8 6A 00 6A 01 E8 [0x43AC00] E8 [0x41D450] 68 <codecave:Dai.RandomAttack.th14.pointdevice.enemy_snapshot> 56 E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_enemy] 68 <codecave:Dai.RandomAttack.th14.pointdevice.player_snapshot> FF35 <0x4DB67C> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_player] 68 <codecave:Dai.RandomAttack.th14.pointdevice.item_manager_snapshot> FF35 <0x4DB660> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_item_manager] 68 <codecave:Dai.RandomAttack.th14.pointdevice.bullet_manager_snapshot> FF35 <0x4DB530> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_bullet_manager] 83EC 0C 8B35 <0x4DB528> F30F7E86 74420000 660FD60424 8B86 7C420000 894424 08 68 <codecave:Dai.RandomAttack.th14.pointdevice.stage_background_snapshot> 56 E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_stage_background] F30F1086 74420000 F30F5C0424 F30F1186 78430000 F30F1086 78420000 F30F5C4424 04 F30F1186 7C430000 F30F1086 7C420000 F30F5C4424 08 F30F1186 80430000 83C4 0C 68 <codecave:Dai.RandomAttack.th14.pointdevice.globals_snapshot> 68 <0x4F5828> E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_globals] A1 <codecave:Dai.RandomAttack.th14.pointdevice.game_speed_snapshot> A3 <0x4D8F58> 8B0D <0x4DB67C> E8 [0x44F970] 8B0D <0x4DB550> FF35 <0x4F5874> FF35 <0x4F5870> E8 [0x434FD0] 8B0D <0x4F56CC> E8 [codecave:Dai.RandomAttack.th14.pointdevice.remove_vms_with_flag_set] 5B 5F 5E C3",
         },
         
         "Dai.RandomAttack.th14.pointdevice.copy_struct_part": {
@@ -125,12 +165,12 @@
         "Dai.RandomAttack.th14.pointdevice.copy_item_manager": {
             //arg1 = target, arg2 = source
             "access": "re",
-            "code": "55 89E5 56 57 8B75 0C 8B7D 08  68 88D8DD00 6A 10 56 57 E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_struct_part]  5F 5E 89EC 5D C2 0800"
+            "code": "55 89E5 56 57 8B75 0C 8B7D 08  68 (0x1C5888 + (<option:bullet-cap.cancel-cap> * 0xC18)) 6A 10 56 57 E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_struct_part]  5F 5E 89EC 5D C2 0800"
         },
         "Dai.RandomAttack.th14.pointdevice.copy_bullet_manager": {
             //arg1 = target, arg2 = source
             "access": "re",
-            "code": "55 89E5 56 57 8B75 0C 8B7D 08  68 CCF69B00 6A 10 56 57 E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_struct_part]  5F 5E 89EC 5D C2 0800"
+            "code": "55 89E5 56 57 8B75 0C 8B7D 08  68 (0x9C + (<option:bullet-cap.bullet-cap> * 0x13F4)) 6A 10 56 57 E8 [codecave:Dai.RandomAttack.th14.pointdevice.copy_struct_part]  5F 5E 89EC 5D C2 0800"
         },
         "Dai.RandomAttack.th14.pointdevice.copy_stage_background": {
             //arg1 = target, arg2 = source
@@ -151,6 +191,22 @@
         "Dai.RandomAttack.th14.pointdevice.dont_display_spell_failed_popups_if_reload": {
             "access": "re",
             "code": "833D <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 02 0F84 [0x41D5A1] E9 [0x41D584]",
+        },
+        
+        "Dai.RandomAttack.th14.pointdevice.dont_reset_stage_camera_vec3_104_if_reload": {
+            "access": "re",
+            "code": "833D <codecave:Dai.RandomAttack.th14.pointdevice.snapshot_status> 03 0F84 [0x40D9FF] F30F7E4424 3C 660FD686 78430000  8B4424 44 8986 80430000 E9 [0x40D9FF]",
+        },
+        
+        "Dai.RandomAttack.th14.pointdevice.custom_anm_ins": {
+            //ins 2000 deleteOnReload() - sets a flag that causes the anm VM to delete itself upon reload
+            "access": "re",
+            "code": "3D 77050000 0F85 [0x472AB9] 8B43 08 8987 A4040000 E9 [0x472AB9]"
+        },
+        "Dai.RandomAttack.th14.pointdevice.remove_vms_with_flag_set": {
+            //flag set by ins 2000 deleteOnReload
+            "access": "re",
+            "code": "8B81 0882FE00 85C0 74 18 8B10 85D2 74 0D 83BA A4040000 01 75 04 834A 1C 20 8B40 04 EB E4 8B81 1082FE00 85C0 74 18 8B10 85D2 74 0D 83BA A4040000 01 75 04 834A 1C 20 8B40 04 EB E4 C3"
         },
         
         "Dai.RandomAttack.th14.replace_life_piece": {
